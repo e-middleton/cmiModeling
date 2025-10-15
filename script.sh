bd9fd73086f915ad735e1ea7309e56bb59b0ef68
Emily Middleton <emiddleton@smith.edu>
2025-10-15 12:45:37 -0400

    if-else logic to check if files need to be shuffled after the model is run

--- script.sh
+++ script.sh
@@ -1,6 +1,39 @@
 #! /bin/bash
 
-saveFigs="--saveFigures"
-testName="--testName=test"
 
-python3 main.py $saveFigs $testName
+saveFigs="True"
+saveData="True"
+testFile="test2"
+testName="--testName=$testFile"
+
+# check if test file directory exists
+if [ -d "./_outputs/$testFile" ] ; then 
+echo "test directory already exists, not recreating"
+else
+echo "creating test directory"
+cd _outputs
+mkdir $testFile
+cd ..
+fi # end the if-else statement
+
+if [ "$saveFigs" == "True" ] && [ "$saveData" == "True" ] # run the model, and save figures if that tag is set
+then
+    echo "Save figures is true"
+    echo "Save data is true"
+    python3 main.py "--saveFigures" "--saveData" $testName
+    mv *.pdf ./_outputs/$testFile/
+    mv *.txt ./_outputs/$testFile/
+
+elif [ "$saveFigs" == "True" ]; then
+    echo "save figures is true"
+    python3 main.py "--saveFigures" $testName
+    mv *.pdf ./_outputs/$testFile/
+
+elif [ "saveData" == "True" ]; then
+    echo "save data is true"
+    python3 main.py "--saveData" $testName
+    mv *.txt ./_outputs/$testFile/
+else
+    python3 main.py $testName
+fi
+
