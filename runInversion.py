42773dae450ec797616fa9fee699f3a2bc96384b
Emily Middleton <emiddleton@smith.edu>
2025-11-19 09:42:07 -0500

    initial commit method for reinverting without afterslip

--- runInversion.py
+++ runInversion.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 # assemble the weights matrix, inputing the smoothing weights for the fault, cmi, and the weights for the 
@@ -9,9 +10,38 @@ def assembleWeights(numMeshes, assembledMat, dispArray, smoothingWeights, allEle
     weights = np.ones((np.shape(assembledMat)[0], 1)) # might want to update when adding slip constraint
 
     for mesh_idx in range(numMeshes+2):
-        weights[np.size(dispArray)+ allElemBegin[mesh_idx]:np.size(dispArray)+allElemEnd[mesh_idx]] = smoothingWeights[mesh_idx]
+        weights[np.size(dispArray) + allElemBegin[mesh_idx]:np.size(dispArray)+allElemEnd[mesh_idx]] = smoothingWeights[mesh_idx]
 
     return weights
+
+# remove the contribution of predicted fault displacements from the observed gps displacements
+def removeFaultContribution(gps, estSlip, dispMat, allElemBegin):
+    # calculate displacements by which components
+
+    # observed displacements only east and north because fault has no tensile slip (?)
+    observedEast = np.array(gps.east_vel)
+    observedNorth = np.array(gps.north_vel)
+    observedUp = np.array(gps.up_vel)
+
+    # calc disp from fault
+    # multiply disp mat by the estimated slip on the fault
+    fault_disp = dispMat[:, allElemBegin[0]:allElemBegin[1]].dot(estSlip[allElemBegin[0]:allElemBegin[1]])
+    fault_e = fault_disp[0::3].flatten()
+    fault_n = fault_disp[1::3].flatten()
+
+    newEast = observedEast - fault_e
+    newNorth = observedNorth - fault_n
+    newUp = observedUp
+
+    # same column headers as old gps file
+    newGps = pd.DataFrame({"station_ID": np.array(gps.station_ID),
+                           "lon": np.array(gps.lon),
+                           "lat": np.array(gps.lat),
+                           "east_vel": newEast,
+                           "north_vel": newNorth,
+                           "up_vel": newUp})
+
+    return newGps
 
 def runInversion(assembledMat, dispMat, weights, dataVector) :
     # Calculate model covariance
