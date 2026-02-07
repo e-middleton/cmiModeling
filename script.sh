a13904407029d2cc8e39292499f06b9f6b9805b7
Emily Middleton <emiddleton@smith.edu>
2026-02-07 11:34:41 -0500

    updated to function for rerunning comparison tests

--- script.sh
+++ script.sh
@@ -1,42 +1,11 @@
 #! /bin/bash
-
-
-saveFigs="True"
-saveData="True"
-testFile="test5"
-testName="--testName=$testFile"
-
-# check if test file directory exists
-if [ -d "./_outputs/$testFile" ] ; then 
-echo "test directory already exists, not recreating"
-else
-echo "creating test directory"
-cd _outputs
-mkdir $testFile
-cd ..
-fi # end the if-else statement
-
-if [ "$saveFigs" == "True" ] && [ "$saveData" == "True" ] # run the model, and save figures if that tag is set
-then
-    echo "Save figures is true"
-    echo "Save data is true"
-    python3 main.py "--saveFigures" "--saveData" $testName
-    mv *.pdf ./_outputs/$testFile/
-    mv numericalResults.txt ./_outputs/$testFile/
-    mv configSettings.txt ./_outputs/$testFile/
-
-elif [ "$saveFigs" == "True" ]; then
-    echo "save figures is true"
-    python3 main.py "--saveFigures" $testName
-    mv *.pdf ./_outputs/$testFile/
-    mv configSettings.txt ./_outputs/$testFile/
-
-elif [ "saveData" == "True" ]; then
-    echo "save data is true"
-    python3 main.py "--saveData" $testName
-    mv numericalResults.txt ./_outputs/$testFile/
-    mv configSettings.txt ./_outputs/$testFile/
-else
-    python3 main.py $testName
-fi
-
+python3 main.py "--oldResults" "--resultFolder=diaoComparisons/D80_SV/D80_SV_f1e12_c1e20"
+mv *.png diaoComparisons/D80_SV/D80_SV_f1e12_c1e20
+python3 main.py "--oldResults" "--resultFolder=diaoComparisons/D80_SV/D80_SV_f5e12_c1e20"
+mv *.png diaoComparisons/D80_SV/D80_SV_f5e12_c1e20
+python3 main.py "--oldResults" "--resultFolder=diaoComparisons/D80_SV/D80_SV_f1e13_c1e20"
+mv *.png diaoComparisons/D80_SV/D80_SV_f1e13_c1e20
+python3 main.py "--oldResults" "--resultFolder=diaoComparisons/D80_SV/D80_SV_f5e13_c1e20"
+mv *.png diaoComparisons/D80_SV/D80_SV_f5e13_c1e20
+python3 main.py "--oldResults" "--resultFolder=diaoComparisons/D80_SV/D80_SV_f1e14_c1e20"
+mv *.png diaoComparisons/D80_SV/D80_SV_f1e14_c1e20
