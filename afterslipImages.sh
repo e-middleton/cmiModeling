87e4919d5380101e520136a9b25d89dd03a32a8f
Emily Middleton <emiddleton@smith.edu>
2026-02-15 16:41:59 -0500

    scripts for file editing and organizing

---
+++ afterslipImages.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+mkdir og_afterslipImages_c21
+cp cmiModeling/_outputsCMI2/D40_SV_Testing/test1/afterslip.pdf ./afterslip1.pdf
+cp cmiModeling/_outputsCMI2/D40_SV_Testing/test5/afterslip.pdf ./afterslip2.pdf
+cp cmiModeling/_outputsCMI2/D40_SV_Testing/test9/afterslip.pdf ./afterslip3.pdf
+cp cmiModeling/_outputsCMI2/D40_SV_Testing/test13/afterslip.pdf ./afterslip4.pdf
+cp cmiModeling/_outputsCMI2/D40_SV_Testing/test17/afterslip.pdf ./afterslip5.pdf
+cp cmiModeling/_outputsCMI2/D50_SV_Testing/test1/afterslip.pdf ./afterslip6.pdf
+cp cmiModeling/_outputsCMI2/D50_SV_Testing/test5/afterslip.pdf ./afterslip7.pdf
+cp cmiModeling/_outputsCMI2/D50_SV_Testing/test9/afterslip.pdf ./afterslip8.pdf
+cp cmiModeling/_outputsCMI2/D50_SV_Testing/test13/afterslip.pdf ./afterslip9.pdf
+cp cmiModeling/_outputsCMI2/D50_SV_Testing/test17/afterslip.pdf ./afterslip10.pdf
+cp cmiModeling/_outputsCMI2/D60_SV_Testing/test1/afterslip.pdf ./afterslip11.pdf
+cp cmiModeling/_outputsCMI2/D60_SV_Testing/test5/afterslip.pdf ./afterslip12.pdf
+cp cmiModeling/_outputsCMI2/D60_SV_Testing/test9/afterslip.pdf ./afterslip13.pdf
+cp cmiModeling/_outputsCMI2/D60_SV_Testing/test13/afterslip.pdf ./afterslip14.pdf
+cp cmiModeling/_outputsCMI2/D60_SV_Testing/test17/afterslip.pdf ./afterslip15.pdf
+cp cmiModeling/_outputsCMI2/D80_SV_Testing/test1/afterslip.pdf ./afterslip16.pdf
+cp cmiModeling/_outputsCMI2/D80_SV_Testing/test5/afterslip.pdf ./afterslip17.pdf
+cp cmiModeling/_outputsCMI2/D80_SV_Testing/test9/afterslip.pdf ./afterslip18.pdf
+cp cmiModeling/_outputsCMI2/D80_SV_Testing/test13/afterslip.pdf ./afterslip19.pdf
+cp cmiModeling/_outputsCMI2/D80_SV_Testing/test17/afterslip.pdf ./afterslip20.pdf
+mv *.pdf ./og_afterslipImages_c21
