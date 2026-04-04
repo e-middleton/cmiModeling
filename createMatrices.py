750de506f5e8e415b57a9cb55ea862afa3f8f719
Emily Middleton <emiddleton@smith.edu>
2026-04-04 16:18:46 -0400

    return to original dispMat and smoothMat creation

--- createMatrices.py
+++ createMatrices.py
@@ -6,85 +6,8 @@ from dataclasses import dataclass
 # create a displacement matrix and a smoothing matrix
 # isFault parameter controls whether or not tensile slip is removed from the matrix and the corresponding rows/columns in smat
 # meshes is a list of length 1
-# def createDispSmoothMats(gps, numTri, beginIndices, endIndices, meshes) :
+def createDispSmoothMats(gps, numTri, beginIndices, endIndices, meshes) :
 
-#     # Quick config class
-#     @dataclass
-#     class Config:
-#         material_lambda = 30000000000
-#         material_mu = 30000000000
-#     config = Config()
-
-#     # Allocate space for slip-to-displacement array
-#     disp_mat = np.zeros((3*len(gps.lon), 3*numTri))
-#     # Allocate space for slip-to-displacement array
-#     smoothing_mat = np.zeros((3*numTri, 3*numTri))
-
-#     # For each mesh, fill in disp_mat with the values and not just zeros
-#     for mesh_idx in range(len(meshes)):
-#         # Calculate slip to displacement partials, using geographic coordinates
-#         disp_mat[:, 3*beginIndices[mesh_idx]:3*endIndices[mesh_idx]] = celeri.spatial.get_tde_to_velocities_single_mesh(meshes, gps, config, mesh_idx)
-#         # Get smoothing operator
-
-#         # Indices of shared sides in this mesh
-#         share = celeri.spatial.get_shared_sides(meshes[mesh_idx].verts)
-#         # Distances between centroids of shared elements
-#         tri_shared_sides_distances = celeri.spatial.get_tri_shared_sides_distances(
-#                 share,
-#                 meshes[mesh_idx].x_centroid,
-#                 meshes[mesh_idx].y_centroid,
-#                 meshes[mesh_idx].z_centroid,
-#             )
-#         # Distance-scaled smoothing matrix
-#         smat = celeri.spatial.get_tri_smoothing_matrix(
-#                 share, tri_shared_sides_distances
-#             )
-#         # Insert sparse matrix into full array
-#         smoothing_mat[3*beginIndices[mesh_idx]:3*endIndices[mesh_idx], 3*beginIndices[mesh_idx]:3*endIndices[mesh_idx]] = smat.toarray()
-#         # Get smoothing operator
-
-#     # get rid of tensile slip columns in fault part of disp_mat and  rows/columns of smoothing matrix (needs to be square)
-#     from celeri import celeri_util
-#     keep = celeri_util.get_keep_index_12(3*endIndices[0])
-
-#     # using the indicies of the elements to be kept, find the elements that need to be deleted
-#     throw = []
-#     for i in range(3*beginIndices[1]):
-#         if np.isin(keep, i).any():
-#             pass
-#         else:
-#             throw.append(i)
-#     throw=np.array(throw)
-
-#     # take out the tensile columns of the subduction zone matrix, leave the CMI matrix as is
-#     # take out tensile rows/columns of smoothing mat, leaving it a square matrix
-
-#     disp_mat = np.delete(disp_mat, throw, axis=1)
-#     smoothing_mat = np.delete(smoothing_mat, throw, axis=0)
-#     smoothing_mat = np.delete(smoothing_mat, throw, axis=1)
-
-#     # test if elem are line elem instead of triangle
-#     # may or may not work, not fully tested
-#     bad_cols = np.isnan(disp_mat[0,:])
-#     if np.sum(bad_cols != 0) :
-#         bad_el = int(np.where(bad_cols)[0][0]/3)
-
-#         raise RuntimeError("Flattened element in one of the meshes creating Nan values in displacement matrix. Bad element = ", bad_el)
-
-#         # thing = bad_el - beginIndices[1]
-#         # print(np.shape(np.unique(meshes[1]["points"][meshes[1]["verts"][thing,:],:],axis=0))[0])
-#         # print(meshes[1]["verts"][thing,:])
-
-#         # # Draw the fault 
-#         # fig, ax = plt.subplots()
-#         # ax.triplot(horiz["points"][:, 0], horiz["points"][:, 1], horiz["verts"], linewidth=0.25)
-#         # ax.plot(horiz["centroids"][thing,0], horiz["centroids"][thing,1], "*r")
-#         # ax.set_aspect("equal")
-#         # plt.show()
-
-#     return disp_mat, smoothing_mat
-
-def createDispSmoothMats(gps, numTri, meshes, isFault=False) :
     # Quick config class
     @dataclass
     class Config:
@@ -97,47 +20,48 @@ def createDispSmoothMats(gps, numTri, meshes, isFault=False) :
     # Allocate space for slip-to-displacement array
     smoothing_mat = np.zeros((3*numTri, 3*numTri))
 
-    # Calculate slip to displacement partials, using geographic coordinates
-    disp_mat[:, 0:3*numTri] = celeri.spatial.get_tde_to_velocities_single_mesh(meshes, gps, config, 0)
-    # Get smoothing operator
+    # For each mesh, fill in disp_mat with the values and not just zeros
+    for mesh_idx in range(len(meshes)):
+        # Calculate slip to displacement partials, using geographic coordinates
+        disp_mat[:, 3*beginIndices[mesh_idx]:3*endIndices[mesh_idx]] = celeri.spatial.get_tde_to_velocities_single_mesh(meshes, gps, config, mesh_idx)
+        # Get smoothing operator
 
-    # Indices of shared sides in this mesh
-    share = celeri.spatial.get_shared_sides(meshes[0].verts)
-    # Distances between centroids of shared elements
-    tri_shared_sides_distances = celeri.spatial.get_tri_shared_sides_distances(
-            share,
-            meshes[0].x_centroid,
-            meshes[0].y_centroid,
-            meshes[0].z_centroid,
-        )
-    # Distance-scaled smoothing matrix
-    smat = celeri.spatial.get_tri_smoothing_matrix(
-            share, tri_shared_sides_distances
-        )
-    # Insert sparse matrix into full array
-    smoothing_mat[0:3*numTri, 0:3*numTri] = smat.toarray()
-    # Get smoothing operator
+        # Indices of shared sides in this mesh
+        share = celeri.spatial.get_shared_sides(meshes[mesh_idx].verts)
+        # Distances between centroids of shared elements
+        tri_shared_sides_distances = celeri.spatial.get_tri_shared_sides_distances(
+                share,
+                meshes[mesh_idx].x_centroid,
+                meshes[mesh_idx].y_centroid,
+                meshes[mesh_idx].z_centroid,
+            )
+        # Distance-scaled smoothing matrix
+        smat = celeri.spatial.get_tri_smoothing_matrix(
+                share, tri_shared_sides_distances
+            )
+        # Insert sparse matrix into full array
+        smoothing_mat[3*beginIndices[mesh_idx]:3*endIndices[mesh_idx], 3*beginIndices[mesh_idx]:3*endIndices[mesh_idx]] = smat.toarray()
+        # Get smoothing operator
 
     # get rid of tensile slip columns in fault part of disp_mat and  rows/columns of smoothing matrix (needs to be square)
-    if (isFault) :
-        from celeri import celeri_util
-        keep = celeri_util.get_keep_index_12(3*numTri)
+    from celeri import celeri_util
+    keep = celeri_util.get_keep_index_12(3*endIndices[0])
 
-        # using the indicies of the elements to be kept, find the elements that need to be deleted
-        throw = []
-        for i in range(3*numTri):
-            if np.isin(keep, i).any():
-                pass
-            else:
-                throw.append(i)
-        throw=np.array(throw)
+    # using the indicies of the elements to be kept, find the elements that need to be deleted
+    throw = []
+    for i in range(3*beginIndices[1]):
+        if np.isin(keep, i).any():
+            pass
+        else:
+            throw.append(i)
+    throw=np.array(throw)
 
-        # take out the tensile columns of the subduction zone matrix, leave the CMI matrix as is
-        # take out tensile rows/columns of smoothing mat, leaving it a square matrix
+    # take out the tensile columns of the subduction zone matrix, leave the CMI matrix as is
+    # take out tensile rows/columns of smoothing mat, leaving it a square matrix
 
-        disp_mat = np.delete(disp_mat, throw, axis=1)
-        smoothing_mat = np.delete(smoothing_mat, throw, axis=0)
-        smoothing_mat = np.delete(smoothing_mat, throw, axis=1)
+    disp_mat = np.delete(disp_mat, throw, axis=1)
+    smoothing_mat = np.delete(smoothing_mat, throw, axis=0)
+    smoothing_mat = np.delete(smoothing_mat, throw, axis=1)
 
     # test if elem are line elem instead of triangle
     # may or may not work, not fully tested
@@ -159,6 +83,82 @@ def createDispSmoothMats(gps, numTri, meshes, isFault=False) :
         # plt.show()
 
     return disp_mat, smoothing_mat
+
+# def createDispSmoothMats(gps, numTri, meshes, isFault=False) :
+#     # Quick config class
+#     @dataclass
+#     class Config:
+#         material_lambda = 30000000000
+#         material_mu = 30000000000
+#     config = Config()
+
+#     # Allocate space for slip-to-displacement array
+#     disp_mat = np.zeros((3*len(gps.lon), 3*numTri))
+#     # Allocate space for slip-to-displacement array
+#     smoothing_mat = np.zeros((3*numTri, 3*numTri))
+
+#     # Calculate slip to displacement partials, using geographic coordinates
+#     disp_mat[:, 0:3*numTri] = celeri.spatial.get_tde_to_velocities_single_mesh(meshes, gps, config, 0)
+#     # Get smoothing operator
+
+#     # Indices of shared sides in this mesh
+#     share = celeri.spatial.get_shared_sides(meshes[0].verts)
+#     # Distances between centroids of shared elements
+#     tri_shared_sides_distances = celeri.spatial.get_tri_shared_sides_distances(
+#             share,
+#             meshes[0].x_centroid,
+#             meshes[0].y_centroid,
+#             meshes[0].z_centroid,
+#         )
+#     # Distance-scaled smoothing matrix
+#     smat = celeri.spatial.get_tri_smoothing_matrix(
+#             share, tri_shared_sides_distances
+#         )
+#     # Insert sparse matrix into full array
+#     smoothing_mat[0:3*numTri, 0:3*numTri] = smat.toarray()
+#     # Get smoothing operator
+
+#     # get rid of tensile slip columns in fault part of disp_mat and  rows/columns of smoothing matrix (needs to be square)
+#     if (isFault) :
+#         from celeri import celeri_util
+#         keep = celeri_util.get_keep_index_12(3*numTri)
+
+#         # using the indicies of the elements to be kept, find the elements that need to be deleted
+#         throw = []
+#         for i in range(3*numTri):
+#             if np.isin(keep, i).any():
+#                 pass
+#             else:
+#                 throw.append(i)
+#         throw=np.array(throw)
+
+#         # take out the tensile columns of the subduction zone matrix, leave the CMI matrix as is
+#         # take out tensile rows/columns of smoothing mat, leaving it a square matrix
+
+#         disp_mat = np.delete(disp_mat, throw, axis=1)
+#         smoothing_mat = np.delete(smoothing_mat, throw, axis=0)
+#         smoothing_mat = np.delete(smoothing_mat, throw, axis=1)
+
+#     # test if elem are line elem instead of triangle
+#     # may or may not work, not fully tested
+#     bad_cols = np.isnan(disp_mat[0,:])
+#     if np.sum(bad_cols != 0) :
+#         bad_el = int(np.where(bad_cols)[0][0]/3)
+
+#         raise RuntimeError("Flattened element in one of the meshes creating Nan values in displacement matrix. Bad element = ", bad_el)
+
+#         # thing = bad_el - beginIndices[1]
+#         # print(np.shape(np.unique(meshes[1]["points"][meshes[1]["verts"][thing,:],:],axis=0))[0])
+#         # print(meshes[1]["verts"][thing,:])
+
+#         # # Draw the fault 
+#         # fig, ax = plt.subplots()
+#         # ax.triplot(horiz["points"][:, 0], horiz["points"][:, 1], horiz["verts"], linewidth=0.25)
+#         # ax.plot(horiz["centroids"][thing,0], horiz["centroids"][thing,1], "*r")
+#         # ax.set_aspect("equal")
+#         # plt.show()
+
+#     return disp_mat, smoothing_mat
 
 
 # pass in the cmi mesh, and new 
