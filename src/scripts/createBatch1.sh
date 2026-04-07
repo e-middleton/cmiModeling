f7683e2215b614a061e8ad847e329377233d3a40
Emily Middleton <emiddleton@smith.edu>
2026-04-07 09:37:17 -0400

    additional fault weights

--- src/scripts/createBatch1.sh
+++ src/scripts/createBatch1.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-# source ~/miniconda3/etc/profile.d/conda.sh 
-# conda activate cmiModeling
+source ~/miniconda3/etc/profile.d/conda.sh 
+conda activate cmiModeling
 
 if [ -d "./_outputs/batch1" ] ; then 
     echo "output directory already exists, not recreating"
@@ -25,7 +25,7 @@ cd ../..
 # --- Tests with spatially uniform smoothing rates --- #
 
 depthValues=(40 50 60 80)
-faultWeights1=(10000000000000 50000000000000 100000000000000 400000000000000 1000000000000000) #1e13-1e15
+faultWeights1=(10000000000000 50000000000000 100000000000000 500000000000000 1000000000000000 5000000000000000 10000000000000000) #1e13-1e16
 cmiWeights1=(10000000000000000 100000000000000000 1000000000000000000 10000000000000000000) #1e16-1e19
 
 for depth in ${depthValues[@]}; do
@@ -54,33 +54,33 @@ for depth in ${depthValues[@]}; do
 done
 
 
-# # # ---- Tests with spatially variable smoothing rates ---- #
+# ---- Tests with spatially variable smoothing rates ---- #
 
-# depthValues2=(40 50 60 80)
-# faultWeights2=(1000000000000 5000000000000 10000000000000 50000000000000 100000000000000) #1e12-1e14
-# cmiWeights2=(10000000000000000 100000000000000000 1000000000000000000 10000000000000000000) #1e16-1e19
+depthValues2=(40 50 60 80)
+faultWeights2=(1000000000000 5000000000000 10000000000000 50000000000000 100000000000000 500000000000000 1000000000000000) #1e12-1e15
+cmiWeights2=(10000000000000000 100000000000000000 1000000000000000000 10000000000000000000) #1e16-1e19
 
-# for depth in ${depthValues2[@]}; do
-#     j=0 #loop variable
-#     for fault in ${faultWeights2[@]}; do 
-#         for cmi in ${cmiWeights2[@]}; do
-#             if [ -d "./_outputs/batch1/D${depth}_SV_Testing/test$j" ] ; then 
-#                 echo "test directory already exists, not recreating"
-#             else
-#                 echo "creating test directory"
-#                 cd "_outputs/batch1/D${depth}_SV_Testing"
-#                 mkdir "test$j"
-#                 cd test$j
-#                 mkdir numpy
-#                 mkdir images
-#                 cd ../../..
-#             fi # end the if-else statement
+for depth in ${depthValues2[@]}; do
+    j=0 #loop variable
+    for fault in ${faultWeights2[@]}; do 
+        for cmi in ${cmiWeights2[@]}; do
+            if [ -d "./_outputs/batch1/D${depth}_SV_Testing/test$j" ] ; then 
+                echo "test directory already exists, not recreating"
+            else
+                echo "creating test directory"
+                cd "_outputs/batch1/D${depth}_SV_Testing"
+                mkdir "test$j"
+                cd test$j
+                mkdir numpy
+                mkdir images
+                cd ../../../..
+            fi # end the if-else statement
 
-#             # by default takes config.yaml as the config file
-#             python main.py "--planeDepth=$depth" "--testName=test$j" "--faultSmoothing=$fault" "--cmiSmoothing=$cmi" "--spatiallyVariable" "--gpsFile=./data/cumulative_disp.txt" "--outputDir=_outputs/batch1/D${depth}_SV_Testing/test${j}/"
+            # by default takes config.yaml as the config file
+            python main.py "--planeDepth=$depth" "--testName=test$j" "--faultSmoothing=$fault" "--cmiSmoothing=$cmi" "--spatiallyVariable" "--gpsFile=./data/cumulative_disp.txt" "--outputDir=_outputs/batch1/D${depth}_SV_Testing/test${j}/"
 
-#             j=$((j+1))
-#             echo # empty line
-#         done
-#     done
-# done
+            j=$((j+1))
+            echo # empty line
+        done
+    done
+done
